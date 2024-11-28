1a764b72e74ea5a03505f293cd82b163db1abdf4
eleochess <leonardo.dcbv@gmail.com>
2024-11-27 20:43:25 -0500

    streamlit update

--- 03streamlit/01write.py
+++ 03streamlit/01write.py
@@ -5,3 +5,13 @@ st.set_page_config(page_title = "Título de la web", page_icon = "😉")
 st.title("Mi segunda aplicación con Streamlit")
 
 st.write("Mi nombre Leonardo Dcbv ^^,")
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns = ["a", "b", "c"])
+
+st.line_chart(chart_data)
+
+st.scatter_chart(chart_data)
+
+st.bar_chart(chart_data)
+
+st.table(chart_data)
