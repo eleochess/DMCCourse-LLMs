273cb66ddbfea712ed4ef5329c50c2f719bbd2ae
eleochess <leonardo.dcbv@gmail.com>
2024-11-27 20:39:08 -0500

    streamlit update

--- 03streamlit/01write.py
+++ 03streamlit/01write.py
@@ -2,6 +2,6 @@ import streamlit as st
  
 st.set_page_config(page_title = "Título de la web", page_icon = "😉")
 
-st.title("Mi primera aplicación con Streamlit")
+st.title("Mi segunda aplicación con Streamlit")
 
-st.write("Mi nombre es *Tony Trujillo* ✌🏻️")
+st.write("Mi nombre Leonardo Dcbv ^^,")
